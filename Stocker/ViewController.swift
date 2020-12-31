6a17ba622321da4292662952b15878da8382f0f2
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2020-12-31 16:29:12 +0900

    init app
    init app

---
+++ Stocker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stocker
+//
+//  Created by 정원영 on 2020/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
