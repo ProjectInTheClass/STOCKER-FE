b4507acf22b3af937155f9fb20356cb07abcb60e
koalakid1 <koalakid154@gmail.com>
2021-01-12 17:52:46 +0900

    fix data storyboard

--- Stocker/Stocker/Views/DataTab/PastDataVC.swift
+++ Stocker/Stocker/Views/DataTab/PastDataVC.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PastDataVC: UITableViewCell {
 
+    @IBOutlet weak var PDview: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -16,7 +18,11 @@ class PastDataVC: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        PDview.layer.cornerRadius = 10
+        PDview.layer.shadowColor = UIColor.black.cgColor
+        PDview.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        PDview.layer.shadowOpacity = 0.2
+        PDview.layer.shadowRadius = 4.0
         // Configure the view for the selected state
     }
 
