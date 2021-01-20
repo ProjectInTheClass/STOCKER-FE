773ac27a4a018bbcc2ae9d92c1c06a5a586236a8
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-01-20 17:00:51 +0900

    Updates

---
+++ precache-manifest.a4804589d02f6e9e4336eaa6ac8bf82d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40e2ceb5d0df6a00496a239c1b91719",
+    "url": "/STOCKER-FE/index.html"
+  },
+  {
+    "revision": "f923d5c96bee4cf8c981",
+    "url": "/STOCKER-FE/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "9a863c104ec4961fc73f",
+    "url": "/STOCKER-FE/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f923d5c96bee4cf8c981",
+    "url": "/STOCKER-FE/static/js/2.72dd8a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/STOCKER-FE/static/js/2.72dd8a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a863c104ec4961fc73f",
+    "url": "/STOCKER-FE/static/js/main.eaa8c725.chunk.js"
+  },
+  {
+    "revision": "2567f3acdec22c3c0cd1",
+    "url": "/STOCKER-FE/static/js/runtime-main.01cd7632.js"
+  },
+  {
+    "revision": "5d7f4679287ffd6e292733efd1fb1538",
+    "url": "/STOCKER-FE/static/media/EverySports.5d7f4679.png"
+  },
+  {
+    "revision": "7c576c39559571f244ad2a74d30d692e",
+    "url": "/STOCKER-FE/static/media/PTPose.7c576c39.gif"
+  },
+  {
+    "revision": "1e4a818199e7cf7f89c5204027d2007b",
+    "url": "/STOCKER-FE/static/media/code.1e4a8181.jpg"
+  },
+  {
+    "revision": "5d7f4679287ffd6e292733efd1fb1538",
+    "url": "/STOCKER-FE/static/media/es_logo.5d7f4679.png"
+  },
+  {
+    "revision": "e663c1a11749bb4dce6618db06ef6709",
+    "url": "/STOCKER-FE/static/media/mainImg2.e663c1a1.png"
+  },
+  {
+    "revision": "9cf4604cff51ee00fe3340b2d612008f",
+    "url": "/STOCKER-FE/static/media/review.9cf4604c.png"
+  },
+  {
+    "revision": "36119ea12f2797cde207ba5093c4aecc",
+    "url": "/STOCKER-FE/static/media/splash.36119ea1.png"
+  }
+]);
